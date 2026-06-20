7333ba677caa0ad39f63cdb063e21f33361f3c1d
tipcode-cpu <tipcode@gmail.com>
2026-06-20 12:21:16 +0900

    PWA: manifest·service-worker·아이콘 추가, index.html에 설치 메타/SW 등록

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'htn-risk-pwa-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './service-worker.js', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
